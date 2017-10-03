7125473dbe53ab519e48a4cb886dd1e26e72c218
samja94 <samja94@gmail.com>
2017-10-03 16:32:35 +0200

    Initial "Person"-class.

---
+++ Casino/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Casino
+{
+    class Person
+    {
+        private const string
+            MSGBOX_ERR_TITLE = "Ett fel uppstod!",
+            EXC_NAME_MSG = "Ange ditt namn!";
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+
+            set
+            {
+                try
+                {
+                    if (string.IsNullOrWhiteSpace(value))
+                        throw new ArgumentNullException("name", EXC_NAME_MSG);
+
+                    name = value;
+                }
+
+                catch (ArgumentNullException e)
+                {
+                    MessageBox.Show(e.ToString(), MSGBOX_ERR_TITLE);
+                }
+            }
+        }
+    }
+}
