58f64b1fe702026e39f1b3e67e65028ea4c27988
samja94 <samja94@gmail.com>
2017-10-03 21:37:16 +0200

    Initial "Wallet"-class.

--- Casino/Wallet.cs
+++ Casino/Wallet.cs
@@ -3,13 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Casino
 {
-    class Wallet
+    class Wallet : Person
     {
-        private decimal wallet;
+        protected decimal wallet;
 
+        public decimal Wallet
+        {
+            get { return wallet; }
 
+            set
+            {
+                try
+                {
+                    if (value <= 0)
+                        throw new ArgumentNullException("name", EXC_NAME_MSG);
+
+                    name = value;
+                }
+
+                catch (ArgumentNullException e)
+                {
+                    MessageBox.Show(e.ToString(), MSGBOX_ERR_TITLE);
+                }
+            }
+        }
     }
 }
