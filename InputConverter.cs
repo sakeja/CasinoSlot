a545b4b9a85972df8c988a67c7fa7998f10c517b
samja94 <samja94@gmail.com>
2017-10-13 12:26:31 +0200

    Inline 'numStr' variable.

--- InputConverter.cs
+++ InputConverter.cs
@@ -10,13 +10,11 @@ namespace CasinoSlot
         // Metod för att konvertera en sträng till ett heltal (uint).
         public static uint TryConvertToUInt32(string s)
         {
-            uint numStr;
-
             /*
              * Om konverteringen kan genomföras, returnera konverterat heltal.
              * Returnera annars konstanten "UInt32.MinValue" som är 0.
              */
-            return (UInt32.TryParse(s, out numStr)) ? numStr : UInt32.MinValue;
+            return (UInt32.TryParse(s, out uint numStr)) ? numStr : UInt32.MinValue;
         }
     }
 }
